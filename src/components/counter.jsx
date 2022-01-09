98e4e44dd7c43353a14571e3b50482120cd44771
chaoui940 <daging129@gmail.com>
2022-01-09 17:38:19 +0100

    C:/Program Files/Git/

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class Counter extends React.Component {
     render() { 
         React.createElement('h1')
-        return <h1>Hello World</h1>;
+        return <div><h1>Hello World</h1><button>Increment</button></div>;
     }
 }
  
